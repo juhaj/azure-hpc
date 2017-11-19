e6c3abee5f8a43bfda0bc3de9712037f04ca1336
Juha Jaykka <juhaj@vega.juhaj.iki.fi>
2017-11-19 00:09:34 +0000

    Fixed processing of command line params

--- slurm-on-sles12-hpc/petschdf5test.py
+++ slurm-on-sles12-hpc/petschdf5test.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3                                                                                                  
 
+import sys
 import numpy
 import petsc4py
+petsc4py.init(sys.argv)
 from petsc4py import PETSc
 
 def initialise(dm, field):
